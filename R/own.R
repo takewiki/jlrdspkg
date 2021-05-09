143b2f00ccec07f0a85b6da5c6625479b3a96f66
takewiki <hulilei@takewiki.com.cn>
2021-05-09 10:19:44 +0800

    fix the ownRpt by bsc

--- R/own.R
+++ R/own.R
@@ -55,6 +55,38 @@ own_getBalanceSheet <- function(conn=tsda::conn_rds('jlrds'),FYear=2020,FMonth=7
   return(res)
 }
 
+
+#' 获取合并报表数据
+#'
+#' @param conn 连接
+#' @param FYear 年份
+#' @param FMonth 月份
+#' @param FUnit 单位
+#' @param digit 小数位数
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' own_getBcsRpt()
+own_getBcsRpt <- function(conn=tsda::conn_rds('jlrds'),FYear=2021,FMonth=3,digit=2){
+  options(scipen = 30,digits = 12)
+  sql <- paste0("select FRptItemNo,FAmount from t_zjrb_bcsRpt where FYear = ",FYear," and FMonth = ",FMonth)
+  res <- tsda::sql_select(conn,sql)
+  #print(res)
+  ncount <-nrow(res)
+  if(ncount >0){
+    res$FAmount <- round(res$FAmount,digit)
+   # print(res)
+
+  }else{
+    res <- NA
+  }
+  return(res)
+}
+
+
+
 #' 获取月份数据
 #'
 #' @param conn 连接
@@ -106,7 +138,7 @@ own_getMonthRpt <- function(conn=tsda::conn_rds('jlrds'),FYear=2020,FMonth=7,FUn
 #'
 #' @examples
 #' own_deal()
-own_deal <- function(conn=tsda::conn_rds('jlrds'),FYear=2020,FMonth=7,FUnit='yuan',digit=2){
+own_deal <- function(conn=tsda::conn_rds('jlrds'),FYear=2021,FMonth=3,FUnit='yuan',digit=2){
    #获取模板数据
    data <- own_getRptTpl(conn=conn)
    #针对数据进行处理
@@ -114,7 +146,9 @@ own_deal <- function(conn=tsda::conn_rds('jlrds'),FYear=2020,FMonth=7,FUnit='yua
    #获取月份数据
    data_monthRpt <- own_getMonthRpt(conn=conn,FYear = FYear ,FMonth = FMonth,FUnit = FUnit,digit = digit)
    #获取资产负债表数据
-   data_bs <- own_getBalanceSheet(conn=conn,FYear=FYear,FMonth = FMonth,FUnit = FUnit,digit=digit)
+   #data_bs <- own_getBalanceSheet(conn=conn,FYear=FYear,FMonth = FMonth,FUnit = FUnit,digit=digit)
+   #获取合并报表数据
+   data_bs <-own_getBcsRpt(conn = conn,FYear = FYear,FMonth = FMonth,digit = digit)
    data$FAmount <-0
    data[data$FRptItemNo =='1','FAmount'] <- as.numeric(NA)
    #处理月份数据
@@ -131,22 +165,22 @@ own_deal <- function(conn=tsda::conn_rds('jlrds'),FYear=2020,FMonth=7,FUnit='yua
      data[data$FRptItemNo =='4','FAmount'] <- as.numeric(NA)
      data[data$FRptItemNo =='5','FAmount'] <- as.numeric(NA)
    }
-   #处理资产负债表数据
+   #处理合并报表数据
    logi_bs <- !is.na(data_bs)
    if(logi_bs){
      #有数据
      #处理应收
-     data[data$FRptItemNo =='7','FAmount'] <- data_bs[data_bs$FRptItemNo =='A1' &data_bs$FCompany=='小计','FAmount']
-     data[data$FRptItemNo =='8','FAmount'] <- data_bs[data_bs$FRptItemNo =='A1' &data_bs$FCompany=='美妆','FAmount']
-     data[data$FRptItemNo =='9','FAmount'] <- data_bs[data_bs$FRptItemNo =='A1' &data_bs$FCompany=='集团','FAmount']
+     data[data$FRptItemNo =='7','FAmount'] <- data_bs[data_bs$FRptItemNo =='A1','FAmount']
+    # data[data$FRptItemNo =='8','FAmount'] <- data_bs[data_bs$FRptItemNo =='A1' &data_bs$FCompany=='美妆','FAmount']
+     # data[data$FRptItemNo =='9','FAmount'] <- data_bs[data_bs$FRptItemNo =='A1' &data_bs$FCompany=='集团','FAmount']
      #其他应收
-     data[data$FRptItemNo =='10','FAmount'] <- data_bs[data_bs$FRptItemNo =='A2' &data_bs$FCompany=='小计','FAmount']
-     data[data$FRptItemNo =='11','FAmount'] <- data_bs[data_bs$FRptItemNo =='A2' &data_bs$FCompany=='美妆','FAmount']
-     data[data$FRptItemNo =='12','FAmount'] <- data_bs[data_bs$FRptItemNo =='A2' &data_bs$FCompany=='集团','FAmount']
+     data[data$FRptItemNo =='10','FAmount'] <- data_bs[data_bs$FRptItemNo =='A2' ,'FAmount']
+    # data[data$FRptItemNo =='11','FAmount'] <- data_bs[data_bs$FRptItemNo =='A2' &data_bs$FCompany=='美妆','FAmount']
+    # data[data$FRptItemNo =='12','FAmount'] <- data_bs[data_bs$FRptItemNo =='A2' &data_bs$FCompany=='集团','FAmount']
      #预收
-     data[data$FRptItemNo =='13','FAmount'] <- data_bs[data_bs$FRptItemNo =='A3' &data_bs$FCompany=='小计','FAmount']
-     data[data$FRptItemNo =='14','FAmount'] <- data_bs[data_bs$FRptItemNo =='A3' &data_bs$FCompany=='美妆','FAmount']
-     data[data$FRptItemNo =='15','FAmount'] <- data_bs[data_bs$FRptItemNo =='A3' &data_bs$FCompany=='集团','FAmount']
+     data[data$FRptItemNo =='13','FAmount'] <- data_bs[data_bs$FRptItemNo =='A3' ,'FAmount']
+    # data[data$FRptItemNo =='14','FAmount'] <- data_bs[data_bs$FRptItemNo =='A3' &data_bs$FCompany=='美妆','FAmount']
+    # data[data$FRptItemNo =='15','FAmount'] <- data_bs[data_bs$FRptItemNo =='A3' &data_bs$FCompany=='集团','FAmount']
      #处理应收未收合计数
      value21 <-data[data$FRptItemNo =='7','FAmount']
      value22 <-data[data$FRptItemNo =='10','FAmount']
@@ -154,17 +188,17 @@ own_deal <- function(conn=tsda::conn_rds('jlrds'),FYear=2020,FMonth=7,FUnit='yua
      data[data$FRptItemNo =='6','FAmount'] = value21 + value22 - value23
 
      #处理应付
-     data[data$FRptItemNo =='17','FAmount'] <- data_bs[data_bs$FRptItemNo =='B1' &data_bs$FCompany=='小计','FAmount']
-     data[data$FRptItemNo =='18','FAmount'] <- data_bs[data_bs$FRptItemNo =='B1' &data_bs$FCompany=='美妆','FAmount']
-     data[data$FRptItemNo =='19','FAmount'] <- data_bs[data_bs$FRptItemNo =='B1' &data_bs$FCompany=='集团','FAmount']
+     data[data$FRptItemNo =='17','FAmount'] <- data_bs[data_bs$FRptItemNo =='B1' ,'FAmount']
+    # data[data$FRptItemNo =='18','FAmount'] <- data_bs[data_bs$FRptItemNo =='B1' &data_bs$FCompany=='美妆','FAmount']
+    # data[data$FRptItemNo =='19','FAmount'] <- data_bs[data_bs$FRptItemNo =='B1' &data_bs$FCompany=='集团','FAmount']
      #处理其他应付
-     data[data$FRptItemNo =='20','FAmount'] <- data_bs[data_bs$FRptItemNo =='B2' &data_bs$FCompany=='小计','FAmount']
-     data[data$FRptItemNo =='21','FAmount'] <- data_bs[data_bs$FRptItemNo =='B2' &data_bs$FCompany=='美妆','FAmount']
-     data[data$FRptItemNo =='22','FAmount'] <- data_bs[data_bs$FRptItemNo =='B2' &data_bs$FCompany=='集团','FAmount']
+     data[data$FRptItemNo =='20','FAmount'] <- data_bs[data_bs$FRptItemNo =='B2' ,'FAmount']
+    # data[data$FRptItemNo =='21','FAmount'] <- data_bs[data_bs$FRptItemNo =='B2' &data_bs$FCompany=='美妆','FAmount']
+     # data[data$FRptItemNo =='22','FAmount'] <- data_bs[data_bs$FRptItemNo =='B2' &data_bs$FCompany=='集团','FAmount']
      #处理预付
-     data[data$FRptItemNo =='23','FAmount'] <- data_bs[data_bs$FRptItemNo =='B3' &data_bs$FCompany=='小计','FAmount']
-     data[data$FRptItemNo =='24','FAmount'] <- data_bs[data_bs$FRptItemNo =='B3' &data_bs$FCompany=='美妆','FAmount']
-     data[data$FRptItemNo =='25','FAmount'] <- data_bs[data_bs$FRptItemNo =='B3' &data_bs$FCompany=='集团','FAmount']
+     data[data$FRptItemNo =='23','FAmount'] <- data_bs[data_bs$FRptItemNo =='B3' ,'FAmount']
+     # data[data$FRptItemNo =='24','FAmount'] <- data_bs[data_bs$FRptItemNo =='B3' &data_bs$FCompany=='美妆','FAmount']
+     # data[data$FRptItemNo =='25','FAmount'] <- data_bs[data_bs$FRptItemNo =='B3' &data_bs$FCompany=='集团','FAmount']
      value31 <-data[data$FRptItemNo =='17','FAmount']
      value32 <-data[data$FRptItemNo =='20','FAmount']
      value33 <-data[data$FRptItemNo =='23','FAmount']
@@ -179,32 +213,32 @@ own_deal <- function(conn=tsda::conn_rds('jlrds'),FYear=2020,FMonth=7,FUnit='yua
      #没有数据
      #处理应收
      data[data$FRptItemNo =='7','FAmount'] <- as.numeric(NA)
-     data[data$FRptItemNo =='8','FAmount'] <- as.numeric(NA)
-     data[data$FRptItemNo =='9','FAmount'] <- as.numeric(NA)
+     #data[data$FRptItemNo =='8','FAmount'] <- as.numeric(NA)
+     #data[data$FRptItemNo =='9','FAmount'] <- as.numeric(NA)
      #其他应收
      data[data$FRptItemNo =='10','FAmount'] <- as.numeric(NA)
-     data[data$FRptItemNo =='11','FAmount'] <-as.numeric(NA)
-     data[data$FRptItemNo =='12','FAmount'] <- as.numeric(NA)
+     #data[data$FRptItemNo =='11','FAmount'] <-as.numeric(NA)
+     #data[data$FRptItemNo =='12','FAmount'] <- as.numeric(NA)
      #预收
      data[data$FRptItemNo =='13','FAmount'] <- as.numeric(NA)
-     data[data$FRptItemNo =='14','FAmount'] <- as.numeric(NA)
-     data[data$FRptItemNo =='15','FAmount'] <- as.numeric(NA)
+     #data[data$FRptItemNo =='14','FAmount'] <- as.numeric(NA)
+     #data[data$FRptItemNo =='15','FAmount'] <- as.numeric(NA)
      #处理应收未收合计数
 
      data[data$FRptItemNo =='6','FAmount'] = as.numeric(NA)
 
      #处理应付
      data[data$FRptItemNo =='17','FAmount'] <- as.numeric(NA)
-     data[data$FRptItemNo =='18','FAmount'] <- as.numeric(NA)
-     data[data$FRptItemNo =='19','FAmount'] <- as.numeric(NA)
+     #data[data$FRptItemNo =='18','FAmount'] <- as.numeric(NA)
+     #data[data$FRptItemNo =='19','FAmount'] <- as.numeric(NA)
      #处理其他应付
      data[data$FRptItemNo =='20','FAmount'] <- as.numeric(NA)
-     data[data$FRptItemNo =='21','FAmount'] <- as.numeric(NA)
-     data[data$FRptItemNo =='22','FAmount'] <- as.numeric(NA)
+     #data[data$FRptItemNo =='21','FAmount'] <- as.numeric(NA)
+     #data[data$FRptItemNo =='22','FAmount'] <- as.numeric(NA)
      #处理预付
      data[data$FRptItemNo =='23','FAmount'] <- as.numeric(NA)
-     data[data$FRptItemNo =='24','FAmount'] <- as.numeric(NA)
-     data[data$FRptItemNo =='25','FAmount'] <- as.numeric(NA)
+     #data[data$FRptItemNo =='24','FAmount'] <- as.numeric(NA)
+     #data[data$FRptItemNo =='25','FAmount'] <- as.numeric(NA)
 
      data[data$FRptItemNo =='16','FAmount'] = as.numeric(NA)
    }
