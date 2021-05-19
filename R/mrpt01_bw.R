7a9b681292baee036a5605fd3dd416f4c4edfe5d
takewiki <hulilei@takewiki.com.cn>
2021-05-19 18:14:04 +0800

    v1

--- R/mrpt01_bw.R
+++ R/mrpt01_bw.R
@@ -1,15 +1,179 @@
 #' 返回SAP报表的固定表头
 #'
+#' @param conn 连接信息从数据库读取
+#'
 #' @return 返回值
 #' @export
 #'
 #' @examples
-#' mrpt_bw_heading()
-mrpt_bw_heading <- function() {
-  res <-c('零售原价','销售收入','销售成本','正品销售成本','其他销售成本(总计)','促销费-折扣折让费用','促销费-折扣折让费用-产成品-商品','促销费-物料配赠费用-外购赠品','促销费-物料配赠费用-产成品-非商品非试','促销费-物料配赠费用-销售物料','促销费-物料配赠费用-产成品-试用装','促销费-物料配赠费用-道具','促销费-物料配赠费用-销售员工物料',
-          '促销费-物料配赠费用-69999','促销费-物料配赠费用-柜台')
+#' mrpt_bw_value()
+mrpt_bw_value <- function(conn=tsda::conn_rds('jlrds')) {
+  # res <-c('零售原价','销售收入','销售成本','正品销售成本','其他销售成本(总计)','促销费-折扣折让费用','促销费-折扣折让费用-产成品-商品','促销费-物料配赠费用-外购赠品','促销费-物料配赠费用-产成品-非商品非试','促销费-物料配赠费用-销售物料','促销费-物料配赠费用-产成品-试用装','促销费-物料配赠费用-道具','促销费-物料配赠费用-销售员工物料',
+  #         '促销费-物料配赠费用-69999','促销费-物料配赠费用-柜台')
+
+  sql <- paste0("select FName_show from t_mrpt_valueType
+where FDataSource='bw'
+order by FIndex")
+  res <- tsda::sql_select(conn,sql)
+  print(res)
+  res <- res$FName_show
   return(res)
 }
+
+
+
+#' 获取BW报表表头
+#'
+#' @param conn 连接
+#' @param solutionNumber 方案号
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' mrpt_bw_value_bySolutionNumber_checked()
+mrpt_bw_value_bySolutionNumber_checked <- function(conn=tsda::conn_rds('jlrds'),solutionNumber='B001') {
+
+
+  sql <- paste0("select FValueName from t_mrpt_rule_bw
+where FSolutionNumber='",solutionNumber,"'")
+  r <- tsda::sql_select(conn,sql)
+  ncount <- nrow(r)
+  if ( ncount >0 ){
+
+    res <- r$FValueName
+
+
+  }else{
+
+    res <- NULL
+  }
+
+  return(res)
+
+}
+
+
+
+
+#' 获取表头信息
+#'
+#' @param conn 连接
+#' @param solutionNumber  方案号
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' mrpt_bw_heading_bySolutionNumber_unChecked()
+mrpt_bw_value_bySolutionNumber_unChecked <- function(conn=tsda::conn_rds('jlrds'),solutionNumber='B001') {
+
+  value_checked <-mrpt_bw_value_bySolutionNumber_checked(conn=conn,solutionNumber = solutionNumber)
+  value_all <- mrpt_bw_value(conn=conn)
+  if (is.null(value_checked)){
+    res <-value_all
+  }else{
+    flag_unchecked <- !value_all %in% value_checked
+    res <- value_all[flag_unchecked]
+  }
+  return(res)
+
+
+}
+
+
+
+#' 返回BW报表的维度信息
+#'
+#' @param conn 连接
+#' @param solutionNumber 方案号
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' mrpt_bw_dim_bySolutionNumber_checked
+mrpt_bw_dim_bySolutionNumber_checked <- function(conn=tsda::conn_rds('jlrds'),solutionNumber='B001') {
+
+
+  sql <- paste0("select FName_sql from t_mrpt_dimType
+where FSulotionNumber ='",solutionNumber,"'")
+  r <- tsda::sql_select(conn,sql)
+  ncount <- nrow(r)
+  if ( ncount >0 ){
+
+    res <- r$FName_sql
+
+
+  }else{
+
+    res <- NULL
+  }
+
+  return(res)
+
+}
+
+
+#' 获取规则表的品牌及渠道
+#'
+#' @param conn 连接
+#' @param solutionNumber 方案号
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' mrpt_bw_rule_brandChannel()
+mrpt_bw_rule_brandChannel <- function(conn=tsda::conn_rds('jlrds'),solutionNumber='B001') {
+
+  sql <- paste0("select  distinct  FBrand_o,FChannel_o   from t_mrpt_rule_bw
+where FSolutionNumber='",solutionNumber,"'")
+  r <- tsda::sql_select(conn,sql)
+  ncount <- nrow(r)
+  if ( ncount >0 ){
+
+    res <- r
+
+
+  }else{
+
+    res <- NULL
+  }
+
+  return(res)
+
+}
+
+
+
+mrpt_bw_deal <- function(file="data-raw/mrpt04/mrpt01_bw/B001_04_data.xlsx",solutionNumber='B001',skip = 1) {
+  #library(readxl)
+  res <- readxl::read_excel(file,skip = skip)
+  #
+  heading_dim <- c('13物料组(物料主数据)','13物料组(物料主数据)-名称','33子渠道（SAP客户组）(客户主数据)','33子渠道（SAP客户组）(客户主数据)-名称','41渠道(分析用)')
+  heading_value <- mrpt_bw_heading()
+
+  heading_total <-c(heading_dim,heading_value)
+  names(res) <- heading_total
+  res <- res[ ,heading_value]
+  res$`方案` <- 'B001'
+  res$`品牌` <-'自然堂'
+  res$`渠道` <-'美妆'
+
+  return(res)
+
+}
+
+
+
+
+
+
+
+
+
+
 
 
 #' BW001报表的处理
