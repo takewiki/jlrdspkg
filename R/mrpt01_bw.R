6bcc65e99b1b75ad5484c5eda2457129aa3e6fc0
takewiki <hulilei@takewiki.com.cn>
2021-05-27 20:16:35 +0800

    UPDATE

--- R/mrpt01_bw.R
+++ R/mrpt01_bw.R
@@ -15,7 +15,7 @@ mrpt_bw_value <- function(conn=tsda::conn_rds('jlrds')) {
 where FDataSource='bw'
 order by FIndex")
   res <- tsda::sql_select(conn,sql)
-  print(res)
+  #print(res)
   res <- res$FName_show
   return(res)
 }
@@ -146,26 +146,162 @@ where FSolutionNumber='",solutionNumber,"'")
 }
 
 
+#' 根据文件获取方案号
+#'
+#' @param file_name 文件名
+#'
+#' @return返回值
+#' @export
+#'
+#' @examples
+#' mrpt_bw_getSolutionByFile()
+mrpt_bw_getSolutionByFile <- function(file_name = "data-raw/mrpt04/mrpt01_bw/B001_04_data.xlsx") {
+  #bb = "data-raw/mrpt04/mrpt01_bw/B001_04_data.xlsx"
+  res = strsplit(file_name,'/')
+  ncount =length(res[[1]])
+  res2 =res[[1]][ncount]
+  res3 = strsplit(res2,'_')
+  res5 = res3[[1]][1]
+  return(res5)
 
-mrpt_bw_deal <- function(file="data-raw/mrpt04/mrpt01_bw/B001_04_data.xlsx",solutionNumber='B001',skip = 1) {
+
+}
+
+#' 获取方案对应的指标变更
+#'
+#' @param conn 连接
+#' @param solutionNumber 方案号
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' mrpt_getValue_selected()
+mrpt_getValue_selected  <- function(conn=tsda::conn_rds('jlrds'),solutionNumber='B001') {
+
+  sql <- paste0("select FValueName  from t_mrpt_rule_bw
+where FSolutionNumber = '",solutionNumber,"'")
+  r <- tsda::sql_select(conn,sql)
+  ncount <- nrow(r)
+  if (ncount >0){
+    res <- r$FValueName
+  }else{
+    res <- NULL
+  }
+  return(res)
+
+}
+#核心处理函数*************——————————————————————————————————
+#' BW报表处理规则
+#'
+#' @param file 文件名
+#' @param conn 连接
+#' @param skip 忽略行
+#' @param FYear 年份
+#' @param FPeriod 月份
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' mrpt_bw_deal()
+mrpt_bw_deal <- function(file="data-raw/mrpt04/BWdata/B001_04.xlsx",
+                         FYear =2021,
+                         FPeriod =4,
+                         conn=tsda::conn_rds('jlrds'),
+
+                         skip = 1) {
   #library(readxl)
   res <- readxl::read_excel(file,skip = skip)
+  #print('bug2')
+  solutionNumber= mrpt_bw_getSolutionByFile(file_name = file)
   #
-  heading_dim <- c('13物料组(物料主数据)','13物料组(物料主数据)-名称','33子渠道（SAP客户组）(客户主数据)','33子渠道（SAP客户组）(客户主数据)-名称','41渠道(分析用)')
-  heading_value <- mrpt_bw_heading()
+  heading_dim <- mrpt_bw_dim_bySolutionNumber_checked(conn = conn,solutionNumber = solutionNumber)
+  heading_value <- mrpt_bw_value()
 
   heading_total <-c(heading_dim,heading_value)
+  #print('bug3')
   names(res) <- heading_total
-  res <- res[ ,heading_value]
-  res$`方案` <- 'B001'
-  res$`品牌` <-'自然堂'
-  res$`渠道` <-'美妆'
+  #print('bug4')
+  #print(names(res))
+  #print(heading_value)
 
-  return(res)
+  res <- res[ ,heading_value]
+  #print('bug1')
+  #names()
+  res$`方案` <- solutionNumber
+  # 处理品牌信息
+   bc = mrpt_bw_rule_brandChannel(conn = conn,solutionNumber = solutionNumber)
+   print(bc)
+  res$`品牌` <-bc$FBrand_o
+  res$`渠道` <-bc$FChannel_o
+  #针对数据进行标准化
+  res2 = reshape2::melt(res,id.vars=c('方案', '品牌', '渠道'))
+  #获取指定数据
+  value_selected = mrpt_getValue_selected(conn = conn,solutionNumber = solutionNumber)
+  #print('bug')
+  #print(value_selected)
+  #print(res2$variable)
+  flag =  res2$variable %in% value_selected
+  #print(flag)
+  res3 = res2[flag, ]
+  #针对数据进一下处理，将0数据处理掉
+  res3$FYear = FYear
+  res3$FPeriod = FPeriod
+  names(res3) <-c('FSolutionNumber','FBrand','FChannel','FValueName','FValue','FYear','FPeriod')
+  #上传数据库
+  ncount2 = nrow(res3)
+  if(ncount2 >0){
+    #写入数据库
+    tsda::db_writeTable(conn = conn,table_name = 't_mrpt_data_bw',r_object = res3,append = T)
+  }
+
+
+
+  return(res3)
+}
+
+#' 针对数据进行批量处理
+#'
+#' @param files 文件名
+#' @param FYear 年份
+#' @param FPeriod 月份
+#' @param conn 连接
+#' @param skip 跳出行
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' mrpt_bw_deal_batch()
+mrpt_bw_deal_batch <- function(files="data-raw/mrpt04/BWdata",
+                               FYear =2021,
+                               FPeriod =4,
+                               conn=tsda::conn_rds('jlrds'),
+
+                               skip = 1) {
+  file_names = dir(files,full.names = TRUE)
+
+
+
+  try({
+    lapply(file_names, function(file){
+      print(file)
+      mrpt_bw_deal(file = file,FYear = FYear,FPeriod = FPeriod,conn = conn,skip = skip)
+
+    })
+  })
+
+
+
+
 
 }
 
 
+
+
+dir("data-raw/mrpt04/BWdata",full.names = TRUE)
 
 
 
--- R/mrpt02_sap.R
+++ R/mrpt02_sap.R
@@ -1,13 +1,20 @@
 #' 针对SAP成本中心进行标准化处理
 #'
 #' @param file 文件名
+#' @param FYear 年份
+#' @param FPeriod 月份
+#' @param conn 连接信息
 #'
 #' @return 返回值
 #' @export
 #'
 #' @examples
 #' sap_costCenter_standardize()
-sap_costCenter_standardize <- function(file="data-raw/rpt_mngr/成本中心划分总表.xlsx") {
+sap_costCenter_standardize <- function(file="data-raw/rpt_mngr/成本中心划分总表 - V3.xlsx",
+                                       FYear =2021,
+                                       FPeriod =4,
+                                       conn=tsda::conn_rds('jlrds')
+                                       ) {
   #library(readxl)
   costCenterRatio <- readxl::read_excel(file,
                                 col_types = c("text", "text", "numeric",
@@ -23,9 +30,103 @@ sap_costCenter_standardize <- function(file="data-raw/rpt_mngr/成本中心划�
   names(mydata) <- c('FcostCenter','FType','FChannel','FValue')
   mydata$FValue <-  round(mydata$FValue /100,2)
   res <- mydata[!is.na(mydata$FValue), ]
-  return(res)
+  #针对数据进行处理
+  bc =as.character(res$FChannel)
+  bc_split = strsplit(bc,"_")
+  bc_res <- lapply(bc_split, function(item){
+    FBrand2 = item[1]
+    FChannel2 =item[2]
+    res <- data.frame(FBrand2,FChannel2,stringsAsFactors = F)
+    return(res)
+  })
+  bc_res2 = do.call('rbind',bc_res)
+  res3 = cbind(res,bc_res2)
+  res3$FYear = FYear
+  res3$FPeriod =FPeriod
+  ncount =nrow(res3)
+  if(ncount >0){
+    tsda::db_writeTable(conn = conn,table_name = 't_mrpt_costCenterRatio_sap',r_object = res3,append = T)
+  }
+
+  return(res3)
   #View(mydata)
   # openxlsx::write.xlsx(mydata,'sapRatio.xlsx')
 }
+
+
+
+
+#' SAP报表读取数据
+#'
+#' @param file 文件
+#' @param FYear 年
+#' @param FPeriod 月
+#' @param conn 连接
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' sap_rpt_readData()
+sap_rpt_readData <- function(file="data-raw/mrpt04/SAPdata/SAP_04_2021V2.XLSX",
+                             FYear =2021,
+                             FPeriod =4,
+                             conn=tsda::conn_rds('jlrds')) {
+
+  #library(readxl)
+  res3 <- readxl::read_excel(file,
+                            col_types = c("date", "date", "text",
+                                          "text", "text", "text", "numeric",
+                                          "text", "text"))
+  names(res3) <-c('FVchDate',	'FPostDate',	'FCostCenterNo',	'FCostObjName',	'FCostItemNumber',
+                 'FCostItemName',
+                 'FRptAmt',	'FVchNote',	'FVchNo')
+  res3$FYear = FYear
+  res3$FPeriod =FPeriod
+  ncount =nrow(res3)
+  if(ncount >0){
+    tsda::db_writeTable(conn = conn,table_name = 't_mrpt_data_sap',r_object = res3,append = T)
+  }
+
+
+
+}
+
+
+#' 写入成本项目数据
+#'
+#' @param file 文件
+#' @param FYear 年
+#' @param FPeriod 月
+#' @param conn 连接
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' sap_rpt_costItem_readData()
+sap_rpt_costItem_readData <- function(file="data-raw/rpt_mngr/SAP成本要素对应关系表.xlsx",
+                             FYear =2021,
+                             FPeriod =4,
+                             conn=tsda::conn_rds('jlrds')) {
+
+  #library(readxl)
+  res3 <- readxl::read_excel(file
+                             )
+  names(res3) <-c('FCostItemName','FRptItemName')
+  res3$FYear = FYear
+  res3$FPeriod =FPeriod
+  ncount =nrow(res3)
+  if(ncount >0){
+    tsda::db_writeTable(conn = conn,table_name = 't_mrpt_costItem_sap',r_object = res3,append = T)
+  }
+
+
+
+}
+
+
+
+
 
 
