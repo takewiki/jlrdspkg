ed8fedb75c3fce8e6b9a4e4ec879df6a9f633ddb
takewiki <hulilei@takewiki.com.cn>
2021-06-19 11:13:53 +0800

    add bw heading name into pkg.

---
+++ R/mrpt01_bw_ui.R
@@ -0,0 +1,21 @@
+
+#' 查看BW报表表头
+#'
+#' @param conn 连接
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' mrpt_bw_ui_getHeadingName()
+mrpt_bw_ui_getHeadingName <- function(conn=tsda::conn_rds('jlrds')) {
+  sql <- paste0("select FInterId,FName_show,FName_sql from t_mrpt_valueType
+where FDataSource='bw'
+order by FIndex")
+  res <- tsda::sql_select(conn,sql)
+  ncount <- nrow(res)
+  if(ncount >0){
+    names(res) <- c('序号','显示名称','存储名称')
+  }
+  return(res)
+}
