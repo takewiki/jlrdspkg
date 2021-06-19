ba0de6dd88b283dba23da5b0fcf57e42a87feb7b
takewiki <hulilei@takewiki.com.cn>
2021-06-19 16:32:04 +0800

    add the bw dim into pkg.

--- R/mrpt01_bw_ui.R
+++ R/mrpt01_bw_ui.R
@@ -19,3 +19,27 @@ order by FIndex")
   }
   return(res)
 }
+
+
+#' BW报表的维度名称
+#'
+#' @param conn 连接
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' mrpt_bw_ui_getDimName()
+mrpt_bw_ui_getDimName <- function(conn=tsda::conn_rds('jlrds')) {
+  sql <- paste0(" select   FSulotionNumber,FIndex,FName_show,FName_sql   from t_mrpt_dimType")
+  res <- tsda::sql_select(conn,sql)
+  ncount <- nrow(res)
+  if(ncount >0){
+    names(res) <- c('方案号','序号','显示名称','存储名称')
+  }
+  return(res)
+}
+
+
+
+
